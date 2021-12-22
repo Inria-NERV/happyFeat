289ab9489c477e0003c59c67694a043bbbd583d4
Arthur D <arthur.desbois@gmail.com>
2021-12-22 13:57:17 +0100

    Generation: electrode list mgmt (button+lineedit) Settings overhaul

--- bcipipeline_qt.py
+++ bcipipeline_qt.py
@@ -5,7 +5,8 @@ import time
 import numpy as np
 
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import QApplication, QMessageBox, QLabel, QHBoxLayout, QComboBox
+from PyQt5.QtWidgets import QApplication, QMessageBox, QLabel, QHBoxLayout, QComboBox, QFileDialog
+from PyQt5.QtWidgets import QWidget
 from PyQt5.QtWidgets import QDialog
 from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtWidgets import QFormLayout
@@ -23,12 +24,18 @@ class Dialog(QDialog):
 
         super().__init__(parent)
 
+        # GENERATION, TEMPLATES, ETC.
+        self.jsonfilename = "params.json"
+        self.generatedFolder = "generated"
+        self.templateFolder = None
+
+        # SCENARIO PARAMETERS...
         self.options = settings.options
         self.optionsNbParams = settings.optionsNbParams
+        self.parameterList = []
+        self.electrodesList = ['Fp1', 'Fp2', 'F7', 'F3', 'Fz', 'F4', 'F8', 'FC5', 'FC1', 'FC2', 'FC6', 'T7', 'C3', 'Cz', 'C4', 'T8', 'TP9', 'CP5', 'CP1', 'CP2', 'CP6', 'TP10', 'P7', 'P3', 'Pz', 'P4', 'P8', 'PO9', 'O1', 'Oz', 'O2', 'PO10']
 
-        jsonfilename = 'C:\\Users\\arthur.desbois\\Documents\\dev\\openvibeScripting\\openvibe-automation\\spectralpower-templates\\params.json'
-        readJsonFile(jsonfilename)
-
+        # INTERFACE INIT...
         self.setWindowTitle('BCI Pipeline - Scenario generation')
         self.dlgLayout = QVBoxLayout()
 
@@ -38,6 +45,7 @@ class Dialog(QDialog):
 
         self.formLayout = QFormLayout()
 
+        self.selectedScenarioName = None
         self.combo = QComboBox(self)
         for i in range(len(self.options)):
             self.combo.addItem(self.options[i], i)
@@ -45,9 +53,20 @@ class Dialog(QDialog):
         self.formLayout.addWidget(self.combo)
 
         self.paramWidgets = []
-        self.parameterList = []
-        self.selectedScenarioName = []
 
+        # Electrodes file...
+        self.electrodesFile = None
+        self.btn_browse = QPushButton("Browse")
+        self.btn_browse.clicked.connect(lambda: self.browseForElectrodeFile())
+        self.electrodesFileWidget = QWidget()
+        layout_h = QHBoxLayout(self.electrodesFileWidget)
+        self.electrodesFileTextBox = QLineEdit()
+        self.electrodesFileTextBox.setText(";".join(self.electrodesList))
+        self.electrodesFileTextBox.setEnabled(True)
+        layout_h.addWidget(self.electrodesFileTextBox)
+        layout_h.addWidget(self.btn_browse)
+
+        # Generate button
         self.btn_generate = QPushButton("Generate scenarios")
         self.btn_generate.clicked.connect(lambda: self.generate())
 
@@ -55,18 +74,24 @@ class Dialog(QDialog):
         self.dlgLayout.addLayout(self.formLayout)
         self.setLayout(self.dlgLayout)
 
-
         # display initial layout
         self.initialWindow()
 
     def comboBoxChanged(self, ix):
         if ix:
-            # TODO : replace by "reset"
+            # TODO : replace by "reset" later on ?
             self.combo.setEnabled(False)
+
+            self.templateFolder = settings.optionsTemplatesDir[ix]
+            print(str("TEMPLATE FOLDER : " + self.templateFolder))
 
             self.selectedScenarioName = self.combo.currentText()
             formLayout = QFormLayout()
 
+            # PARAMETER ALWAYS PRESENT : LIST OF CHANNELS
+            formLayout.addRow("Electrode names file", self.electrodesFileWidget)
+
+            # GET PARAMETER LIST FOR SELECTED BCI PIPELINE, AND DISPLAY THEM
             for i in range(len(settings.scenarioSettings[ix])):
                 # init params...
                 parameter = [settings.scenarioSettings[ix][i][0], settings.scenarioSettings[ix][i][1]]
@@ -74,7 +99,7 @@ class Dialog(QDialog):
                 # create widgets...
                 paramWidget = QLineEdit()
                 paramWidget.setText(str(settings.scenarioSettings[ix][i][1]))
-                settingLabel = str(settings.scenarioSettings[ix][i][0])
+                settingLabel = str(settings.scenarioSettings[ix][i][2])
                 self.paramWidgets.append(paramWidget)
                 formLayout.addRow(settingLabel, self.paramWidgets[i])
 
@@ -82,19 +107,53 @@ class Dialog(QDialog):
         self.dlgLayout.addWidget(self.btn_generate)
         self.show()
 
+    def browseForElectrodeFile(self):
+        directory = os.getcwd()
+        self.electrodesFile, dummy = QFileDialog.getOpenFileName(self, "Open electrode names file", str(directory))
+        if self.electrodesFile:
+            electrodes = []
+            with open(self.electrodesFile) as f:
+                lines = f.readlines()
+                for elec in lines:
+                    electrodes.append(elec.rstrip("\n"))
+            self.electrodesFileTextBox.setText(";".join(electrodes))
+
     def initialWindow(self):
         self.show()
 
     def generate(self):
-
-        #update params...
+        # PIPELINE DEPENDENT : update params from text fields...
         for i in range(len(self.paramWidgets)):
             self.parameterList[i][1] = self.paramWidgets[i].text()
 
-        #generateScenarios(self.selectedScenarioName, self.parameterList)
+        # ALL PIPELINES : Electrode list...
+        electrodes = None
+        if self.electrodesFileTextBox.text() == "":
+            msg = QMessageBox()
+            msg.setText("Please enter a valid file containing electrode names")
+            msg.exec_()
+            return
+        else:
+            electrodes = self.electrodesFileTextBox.text()
+
+        self.parameterList.append(["ChannelNames", electrodes])
+        print(self.parameterList)
+
+        # WRITE JSON PARAMETERS FILE
+        # TODO !
+        jsonfullpath = os.path.join(os.getcwd(), self.templateFolder, self.jsonfilename)
+        readJsonFile(jsonfullpath)
+
+        # GENERATE SC1 (ACQ/MONITOR) + SC2 (FEATURE EXT) + SC2 (TRAIN) + SC3 (ONLINE)
+        # generateScenarios(self.selectedScenarioName, self.parameterList)
+        for filename in settings.templateScenFilenames:
+            srcFile = os.path.join(os.getcwd(), self.templateFolder, filename)
+            destFile = os.path.join(os.getcwd(), self.generatedFolder, filename)
+            print("Copying file " + srcFile + " to " + destFile)
+            # ...
 
         text = "Thanks for using the generation script!\nYour files are in " + os.getcwd() + "/generated/"
-        text += "\n\nDon't forget to double check the generated scenarios...!\nYou can now close this window."
+        text += "\n\n(Don't forget to double check the generated scenarios...!)\nYou can now close this window."
 
         msg = QMessageBox()
         msg.setText(text)
--- bcipipeline_settings.py
+++ bcipipeline_settings.py
@@ -21,16 +21,30 @@ global templateScenFilenames
 templateScenFilenames = ["sc1-monitor-acq.xml",
                          "sc2-extract-select.xml",
                          "sc2-train.xml",
-                         "sc3-online.xml",
-                         "mi-stimulations.lua"]
+                         "sc3-online.xml"]
 
 global scenarioSettings
-# Format : pairs ("setting name" , default value)
+# Format : pairs ("setting name" , default value, "setting description")
 # 1 : None
 # 2 : Power Spectrum based classification
 # 3 : Connectivity
 scenarioSettings = [None,
-                    [("Trial Nb", 20), ("Stimulation 1", "LEFT"), ("Stimulation 2", "RIGHT")],
-                    [("setting1", 1), ("setting2", 2), ("setting3", 3)]]
+                    # POWER SPECTRUM
+                    [("TrialNb", 20, "Number of Trials"),
+                     ("Stim1", "LEFT", "Stimulation 1"),
+                     ("Stim2", "RIGHT", "Stimulation 2"),
+                     ("Baseline", 10, "Baseline duration (s)"),
+                     ("TrialWait", 1.5, "Wait for instruction (s)"),
+                     ("TrialLength", 1.5, "Instruction duration (s)"),
+                     ("EndTrial", 3, "End of trial duration (s)"),
+                     ("StimulationEpoch", 3, "Feat Extraction: Stimulation epoching (s)"),
+                     ("TimeWindowLength", 0.4, "Feat Extraction: Time Window (Burg) (s)"),
+                     ("TimeWindowShift", 0.028, "Feat Extraction: Time Window overlapping (Burg) (s)"),
+                     ("AutoRegressiveOrder", 19, "Feat Extraction: Burg estimation order"),
+                     ("PsdSize", 500, "PSD Size")],
+                    # CONNECTIVITY
+                    [("setting1", 1, "Setting 1"),
+                     ("setting2", 2, "Setting 2"),
+                     ("setting3", 3, "Setting 3")]]
 
 
