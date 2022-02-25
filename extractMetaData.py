55202652bf212f524b031b53069152bd37e6ffef
Arthur D <arthur.desbois@gmail.com>
2022-02-25 17:16:34 +0100

    Mechanism+Tools for extracting metadata from OV - scenario "toolbox-generate-metadata.xml" to extract metadata (as CSV   file) from a OV signal file - py script (extractMetaData.py) to read sampling frequency and   electrodes list from this metadata file - also in extractMetaData.py, routine to launch   toolbox-generate-metadata.xml in the background, by specifying   designer path and OV signal

---
+++ extractMetaData.py
@@ -0,0 +1,69 @@
+import os
+import sys
+import subprocess
+from shutil import copyfile
+import numpy as np
+import pandas as pd
+import csv
+import time
+
+from modifyOpenvibeScen import *
+
+def generateMetadata(ovFile, openvibeDesigner):
+    # Make a copy of Openvibe scenario, for safe modification...
+    scenName = "toolbox-generate-metadata.xml"
+    toolboxPath = "toolbox-scenarios"
+    srcFile = os.path.join(os.getcwd(), toolboxPath, scenName)
+    destFile = os.path.join(os.getcwd(), toolboxPath, scenName.replace(".xml", "-TEMP.xml"))
+    print("---Copying file " + srcFile + " to " + destFile)
+    copyfile(srcFile, destFile)
+
+    inputParam = ovFile.replace("\\", "/")
+    outputParam = inputParam.replace(".ov", "-META.csv")
+    paramDict = {"EEGData": inputParam, "EEGMetaData": outputParam}
+
+    # Modify scenario I/O
+    modifyScenarioGeneralSettings(destFile, paramDict)
+
+    # launch Openvibe toolbox scenario
+    p = subprocess.Popen([openvibeDesigner, "--no-gui", "--play-fast", destFile],
+                         stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    while True:
+        output = p.stdout.readline()
+        if p.poll() is not None:
+            break
+        if output:
+            print(str(output))
+            if "Application terminated" in str(output):
+                break
+
+    return
+
+def extractMetadata(metaCsv):
+    rawHeader = None
+    with open(metaCsv, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            print(row)
+            rawHeader = row
+
+    if not rawHeader:
+        return None
+
+    sampFreq = int(rawHeader[0].split(':')[1].removesuffix('Hz'))
+    electrodeList = rawHeader[2:-3]
+
+    return sampFreq, electrodeList
+
+
+if __name__ == '__main__':
+
+    openvibeDesigner = "C:\\openvibeTestArthur\\dist\\x64\\Release\\openvibe-designer.cmd"
+
+    testSig = "C:\\Users\\arthur.desbois\\Documents\\dev\\openvibeScripting\\openvibe-automation\\generated\\signals\\motor-imagery.ov"
+    # generateMetadata(testSig, openvibeDesigner)
+
+    testCsv = "C:\\Users\\arthur.desbois\\Documents\\dev\\openvibeScripting\\openvibe-automation\\generated\\signals\\motor-imagery-META.csv"
+    sampFreq, electrodeList = extractMetadata(testCsv)
+    print("Sampling Freq: " + str(sampFreq))
+    print("Electrodes (" + str(len(electrodeList)) + "): " + str(electrodeList))
