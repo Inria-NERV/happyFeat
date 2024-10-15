feaac250e2cae997e7fe4195f1df230edcb1bd47
Arthur D <arthur.desbois@gmail.com>
2024-10-15 14:21:27 +0200

    version update in some files (oops...) + removed print

--- happyfeat/bcipipeline_setup.py
+++ happyfeat/bcipipeline_setup.py
@@ -129,7 +129,6 @@ class Dialog(QDialog):
         self.show()
 
     def initAcqParams(self):
-        print('holaaaaaaaaaaaaaaaaaaaaaa')
         # PARAMETERS
         labelText = [None, None]
         label = [None, None]
