a80bd18b482515bf39cc7a37f3af56026ca0248a
Arthur D <arthur.desbois@gmail.com>
2025-01-21 18:10:10 +0100

    [tutorial] avoid prompting the user for default folder

--- tutorials/physionetTutorial.py
+++ tutorials/physionetTutorial.py
@@ -2,5 +2,5 @@ from mne.datasets import eegbci
 from pathlib import Path
 
 customPath = str(Path.cwd())
-raw_fnames = eegbci.load_data(subjects=[1], runs=[4, 8, 12], path=customPath)
+raw_fnames = eegbci.load_data(subjects=[1], runs=[4, 8, 12], path=customPath, update_path=False)
 
