[bugfix] class2-class1 stuff - Timeflux Threads.py: corrected order of classes sent to R2 computation - featExtract.py: reordered/reworded - Stats: switched back to class2-class1 - Viz: autodetection of MI(red) and REST(blue) for metric curves

happyfeat/timeflux/Threads.py
@@ -336,8 +336,8 @@ class LoadFilesForVizPowSpectrum_Timeflux(QtCore.QThread):
         # Statistical Analysis
         freqs_array = np.arange(0, n_bins, fres)
 
-        Rsquare, signTab = Compute_Rsquare_Map(power_cond2_final[:, :, :(n_bins - 1)],
-                                               power_cond1_final[:, :, :(n_bins - 1)])
+        Rsquare, signTab = Compute_Rsquare_Map(power_cond1_final[:, :, :(n_bins - 1)],
+                                               power_cond2_final[:, :, :(n_bins - 1)])
 
         print(electrodeList)
         # Reordering for R map and topography...