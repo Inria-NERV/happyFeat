2908232671484301fc49f89b618a67552ffb4c7f
Arthur Desbois <arthur.desbois@gmail.com>
2025-05-22 13:47:00 +0200

    [bcinet] Reject FT9/FT10/Iz channels for connectivity

--- happyfeat/lib/Visualization_Data.py
+++ happyfeat/lib/Visualization_Data.py
@@ -423,6 +423,9 @@ def plot_Rsquare_plotly(Rsquare, channel_array, freq, fres, each_point,
     return fig
 
 def Reorder_plusplus(Rsquare, signTab, electrodes_orig, powerLeft, powerRight, timefreqLeft, timefreqRight):
+    
+    # TODO : switch between reordering cases in a smarter way!
+    
     if len(electrodes_orig) == 74:
         # NETBCI...
         # electrodes_target = ['fp1', 'af7', 'af3', 'f7', 'f5', 'f3', 'f1', 'ft9', 'ft7', 'fc5', 'fc3', 'fc1',
@@ -445,8 +448,10 @@ def Reorder_plusplus(Rsquare, signTab, electrodes_orig, powerLeft, powerRight, t
                              'PO3', 'POz', 'PO4',  'PO8', 'PO10', 'O1',
                              'Oz',   'O2',  'O9',   'IZ', 'O10']
 
-    elif len(electrodes_orig) >= 64:
+    elif len(electrodes_orig) >= 64:  # typical case...?
         electrodes_target = ['Fp1','AF7','AF3','F7','F5','F3','F1','FT9','FT7','FC5','FC3','FC1','T7','C5','C3','C1','TP7','CP5','CP3','CP1','P7','P5','P3','P1','PO7','PO3','O1','Fpz','AFz','Fz','FCz','Cz','CPz','Pz','POz','Oz','Iz','Fp2','AF8','AF4','F8','F6','F4','F2','FT10','FT8','FC6','FC4','FC2','T8','C6','C4','C2','TP8','CP6','CP4','CP2','P8','P6','P4','P2','PO8','PO4','O2']
+    elif len(electrodes_orig) == 61:  # BCINET, without electrodes FT9, FT10, Iz
+        electrodes_target = ['Fp1','AF7','AF3','F7','F5','F3','F1','FT7','FC5','FC3','FC1','T7','C5','C3','C1','TP7','CP5','CP3','CP1','P7','P5','P3','P1','PO7','PO3','O1','Fpz','AFz','Fz','FCz','Cz','CPz','Pz','POz','Oz','Fp2','AF8','AF4','F8','F6','F4','F2','FT8','FC6','FC4','FC2','T8','C6','C4','C2','TP8','CP6','CP4','CP2','P8','P6','P4','P2','PO8','PO4','O2']
     else:
         electrodes_target = ['Fp1', 'F7', 'F3', 'FC5', 'FC1', 'T7', 'C3', 'CP5', 'CP1', 'P7', 'P3', 'PO9', 'O1', 'AFz',
                              'Fz', 'FCz', 'Cz', 'Pz', 'Oz', 'Fp2', 'F8', 'F4', 'FC6', 'FC2', 'T8', 'C4', 'CP6', 'CP2',
